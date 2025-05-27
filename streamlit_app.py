d6480bbb48fe15007e97adffc947e30385c433dc
renata-starostka <renata.starostka@gmail.com>
2025-05-27 08:55:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Poo to Power (Anaerobic Digestion)")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This app is designed to help wastewater operators of anaerobic digestion systems determine what they should add to their anaerobic digesters. It is a combination of the Anaerobic Digestion Model 1, originally in Matlab, converted to C++ with a petsc solver, and now fully in Python (still using petsc)."
 )
